41fc18447b397385994bbc95462e075821e15617
Matheus <etec.matheus@gmail.com>
2022-11-12 19:56:22 -0300

    feat: Business section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Button() {
-  return (
-    <div>
-
-    </div>
-  )
+interface ButtonProps {
+  styles?: string;
+  value: string;
 }
 
-export default Button
+function Button({ styles, value }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default Button;
