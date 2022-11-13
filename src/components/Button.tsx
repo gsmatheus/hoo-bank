0a15e51ec458d23f9ab94172ed75ee560380528c
Matheus <etec.matheus@gmail.com>
2022-11-13 14:52:49 -0300

    feat: CardDeal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ function Button({ styles, value }: ButtonProps) {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     >
       {value}
     </button>
